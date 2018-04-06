2faf7562c52b35fd2934bb426faee63f63345751
Artem_Pryzhkov <artem_pryzhkov@epam.com>
2018-04-06 14:15:37 +0300

    add user status transition examples for "get" and "list" methods

---
+++ src/user/get-user-transition.js
@@ -0,0 +1,16 @@
+var Hyperwallet = require("hyperwallet-sdk");
+var Utils = require("../utils.js");
+
+var username = process.argv[2];
+var password = process.argv[3];
+var programToken = process.argv[4] || "prg-44a64920-45fc-4578-a7f9-e14813b3ed54";
+var userToken = process.argv[5] || "usr-0fb6d624-f18d-480d-b1e7-f48f534bbd75";
+var statusTransitionToken = process.argv[6] || "";
+
+var client = new Hyperwallet({
+    username: username,
+    password: password,
+    programToken: programToken,
+});
+
+client.getUserStatusTransition(userToken, statusTransitionToken, Utils.printResponse);
---
+++ src/user/list-user-transitions.js
@@ -0,0 +1,15 @@
+var Hyperwallet = require("hyperwallet-sdk");
+var Utils = require("../utils.js");
+
+var username = process.argv[2];
+var password = process.argv[3];
+var programToken = process.argv[4] || "prg-44a64920-45fc-4578-a7f9-e14813b3ed54";
+var userToken = process.argv[5] || "usr-0fb6d624-f18d-480d-b1e7-f48f534bbd75";
+
+var client = new Hyperwallet({
+    username: username,
+    password: password,
+    programToken: programToken,
+});
+
+client.listUserStatusTransitions(userToken, {}, Utils.printResponse);
